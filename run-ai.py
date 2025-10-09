0fcd9e0b65cd2dcc86599f635a83efb60fab4f50
Ayfri <pierre.ayfri@gmail.com>
2025-10-09 09:42:29 +0200

    fix(argparser): Correct argument names to use hyphens instead of underscores.

--- run-ai.py
+++ run-ai.py
@@ -7,11 +7,11 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 def main() -> None:
 	argparser = argparse.ArgumentParser()
-	argparser.add_argument("--population_size", type=int, default=60)
-	argparser.add_argument("--mutation_rate", type=float, default=0.05)
-	argparser.add_argument("--mutation_strength", type=float, default=0.020)
-	argparser.add_argument("--load_latest_generation_weights", action="store_true")
-	argparser.add_argument("--show_window", action="store_true")
+	argparser.add_argument("--population-size", type=int, default=60)
+	argparser.add_argument("--mutation-rate", type=float, default=0.05)
+	argparser.add_argument("--mutation-strength", type=float, default=0.020)
+	argparser.add_argument("--load-latest-generation-weights", action="store_true")
+	argparser.add_argument("--show-window", action="store_true")
 	argparser.add_argument("--checkpoints", action="store_true", help="Use checkpoints as spawn points")
 	args = argparser.parse_args()
 
