1f9ef4db9d51e315266d75e5bcea12ed61a0af6d
Ayfri <pierre.ayfri@gmail.com>
2025-10-31 09:21:22 +0100

    refactor(network): Remove unused convolutional network classes to streamline codebase.

--- ai/neural_network.py
+++ ai/neural_network.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class NeuralNetwork(nn.Module):
 	def __init__(self) -> None:
 		super(NeuralNetwork, self).__init__()
-		self.fc1 = nn.Linear(9 * 9 * 2, 256)
+		self.fc1 = nn.Linear(7 * 7 * 2, 256)
 		self.fc2 = nn.Linear(256, 128)
 		self.fc3 = nn.Linear(128, 64)
 		self.fc4 = nn.Linear(64, 3)  # 3 directions: up, left, right
